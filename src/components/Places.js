5791bc2187f9c058e90d34c8415f3d8731a1d005
ruthw <rwustenberg@gmail.com>
2019-09-02 05:28:27 -0500

    060201

---
+++ src/components/Places.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class Places extends React.Component {
+	state = [{title: 'Igloo',
+						price: 1000,
+						location:'Antarctica'
+					},{title: 'Tent',
+						price: 20,
+						location:'Thailand'
+					},{title: 'Ski Lodge',
+						price: 350,
+						location:'Iceland'
+					},{title: 'House',
+						price: 200,
+						location:'New Zealand'
+					},{title: 'Cave',
+						price: 50,
+						location:'Australia'
+					},{title: 'Mansion',
+						price: 600,
+						location:'Greece'
+					}]
+
+	render () {
+		return
+	}
+}
+
+export default Places
